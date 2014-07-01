4d3b91025cdac6e91b5d8894163541c156d73378
JenGates3255 <Jengates3255@gmail.com>
2014-06-30 20:06:19 -0600

    complete

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).on('ready', function() {
+
+var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
+
+	// for(var i=0; i<animals.length; i++) {
+ // 			console.log(animals[i]);
+	// }
+
+	// for(var i=0; i<animals.length-1; i++){
+	// 		console.log(animals[i]);
+	// 	}
+ // 	for(var i=0; i<animals.length; i++) {
+ // 		if (!(i % 2)){
+ // 			console.log(animals[i]);
+ // 		}	
+	  // }
+	for(var i=animals.length; i; i--){
+	 		console.log(animals[i-1]);
+	 }
+
+	// for (var i=0 ;i<animals.length; i++) {
+	//  	if (i!==0 && i!==animals.length-1){
+	//  		console.log(animals[i]);
+	//  	}
+	//  	console.log(animals[i]);
+	 // };
+});
